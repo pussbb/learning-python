ae8497a6a4a468f0af2615e13dd0e905c6cd0cf8
pussbb <pussbb@gmail.com>
2015-04-30 16:01:32 +0300

    tail -f functionality

--- tail/tail.py
+++ tail/tail.py
@@ -16,6 +16,8 @@ PosixStatResultBase = collections.namedtuple(
      'st_atime', 'st_mtime', 'st_ctime']
 )
 
+TailResults = collections.namedtuple('TailResults', ['lines', 'last_position'])
+
 
 class PosixStatResult(PosixStatResultBase):
     def __getattr__(self, item):
@@ -30,25 +32,33 @@ class PosixStatResult(PosixStatResultBase):
         return cls(*os.stat(absolute_file_name))
 
 
-def tail(file_name, last=10):
+def tail(file_name, last=10, last_pos=0):
     result = []
     if PosixStatResult.stats(file_name).size == 0:
         return result
     with open(file_name, 'r') as fd:
-        fd.seek(0, 2)
-        pos = fd.tell()
-        while True:
-            pos -= pos // last
-            if pos < last or pos < 0:
-                pos = fd.tell()
-            fd.seek(0 - pos, 2)
+
+        if last_pos:
+            fd.seek(last_pos)
             result = fd.readlines()
-            if len(result) >= last or pos == fd.tell():
-                break
+            return TailResults(result, fd.tell())
+        else:
+            fd.seek(0, 2)
+            pos = fd.tell()
+            while True:
+                pos -= pos // last
+                if pos < last or pos < 0:
+                    pos = fd.tell()
+                fd.seek(0 - pos, 2)
+                result = fd.readlines()
+                if len(result) >= last or pos == fd.tell():
+                    break
+        file_pos = fd.tell()
+
     index = len(result) - last
     if index < 0:
         index = 0
-    return result[index:]
+    return TailResults(result[index:], file_pos)
 
 
 def test():
@@ -56,15 +66,28 @@ def test():
     import subprocess
 
     for dir_path, _, files in os.walk('/var/log/'):
-        for _ in range(5):
+        for __ in range(5):
             file_ = os.path.join(dir_path, random.choice(files))
-            lines = random.randint(10, 120)
+            line_num = random.randint(10, 120)
             print('Checking ', file_)
-            assert b''.join(tail(file_, lines)) == subprocess.check_output(
-                ['tail', '-{0}'.format(lines), file_])
+            assert b''.join(tail(file_, line_num).lines) == subprocess\
+                .check_output(['tail', '-{0}'.format(line_num), file_])
 
         break
 
 
+def test2(filename):
+    """ tail -f
+
+    :return:
+    """
+    last_pos = 0
+    while True:
+        result = tail(filename, last_pos=last_pos)
+        last_pos = result.last_position
+        if result.lines:
+            print(''.join(result.lines), end='')
+
 if __name__ == '__main__':
+    test2('/var/log/syslog')
     test()
